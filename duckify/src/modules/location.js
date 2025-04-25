8a03763cf2a1cf0d0b6dfb5268045de143421a37
Kallehallen <75151510+Kallehallen15@users.noreply.github.com>
2025-04-25 08:52:14 +0700

    Delete duckify directory

--- duckify/src/modules/location.js
+++
@@ -1,9 +0,0 @@
-import ExecutionEnvironment from '@docusaurus/ExecutionEnvironment'
-
-let getLanguage = () => 'nope'
-
-if (ExecutionEnvironment.canUseDOM) {
-    getLanguage = () => navigator.language
-}
-
-export default getLanguage
